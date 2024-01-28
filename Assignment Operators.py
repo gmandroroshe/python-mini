940cdb9d33296c53df11f5c6e3ee1007c5b6e3e2
G.M. Kavindu Maduranga <144511926+gmandroroshe@users.noreply.github.com>
2024-01-29 04:44:36 +0530

    Add files via upload

---
+++ Assignment Operators.py
@@ -0,0 +1,23 @@
+x=10
+y=3
+
+# Assignment Operators: 
+# addition:        10+3
+# subtraction:     10-3
+# multiplication:   10*3
+# division:        10/3
+# floor division: 10//3
+# exponent:         10**3
+# modulus:         10%3
+
+
+print (x+y)
+print (x-y)
+print (x*y)
+print (x/y)
+print (x//y)
+print (x%y)
+print (x**y)
+
+
+
