259c8adcd7731df70dfc12b514d2fcf83cc91ecb
G.M. Kavindu Maduranga <144511926+gmandroroshe@users.noreply.github.com>
2024-01-30 06:12:10 +0530

    Add files via upload

---
+++ Loops and Iterations - For,While Loops.py
@@ -0,0 +1,59 @@
+nums = [1,2,3,4,5]
+
+#for num in nums:
+ #   print (num)
+
+
+
+#nums = [1,2,3,4,5]
+
+#for num in nums:
+ #   if num == 3:
+ #       print ('found!')
+ #       break
+  #  print(num)
+
+
+
+
+#nums = [1,2,3,4,5]
+
+#for num in nums:
+ #   if num == 3:
+ #       print ('found!')
+ #       continue
+ #   print(num)
+
+
+
+
+
+#nums = [1,2,3,4,5]
+
+#for num in nums:
+ #   for letter in 'abc':
+  #      print(num, letter)
+
+
+#for I in  range (10):
+ #   print (I)
+
+    
+for I in  range (1, 10):
+    print (I)
+
+
+
+#x = 0 
+#while True:
+   # if x == 5:
+      #  break
+ #   print (x)
+  #  x+= 1
+
+
+
+
+
+
+
